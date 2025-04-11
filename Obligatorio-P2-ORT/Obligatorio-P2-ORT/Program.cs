4cc6a4b0d356ccffdc52d0dc4998c6fdd0683e17
foteiza <fede.oteiza@gmail.com>
2025-04-10 21:43:33 -0300

    Agregando el proyecto

---
+++ Obligatorio-P2-ORT/Obligatorio-P2-ORT/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Obligatorio_P2_ORT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
