acddd2461a834e4e5493d08019f3f9dae14fe4f1
Alfredo Vartabedian <alfredo.vartabedian2@gmail.com>
2025-04-16 21:36:17 -0300

    me pide un commit

--- Obligatorio-P2-ORT/Dominio/Sistema.cs
+++ Obligatorio-P2-ORT/Dominio/Sistema.cs
@@ -9,7 +9,7 @@ namespace Dominio
     public class Sistema
     {
         private List<Usuario> _usuarios = new List<Usuario>();
-        private List<Pasajes> _pasajes = new List<Pasajes>();
+        private List<Pasaje> _pasajes = new List<Pasaje>();
         private List<Vuelo> _vuelos = new List<Vuelo>();
         private List<Ruta> _ruatas = new List<Ruta>();
         private List<Avion> _aviones = new List<Avion>();
