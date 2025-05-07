f1bf4af8f3793338629bd524da73e03f76aed247
Alfredo Vartabedian <alfredo.vartabedian2@gmail.com>
2025-05-07 08:10:57 -0300

    xd

--- Obligatorio-P2-ORT/Dominio/obj/Debug/net8.0/Dominio.AssemblyInfo.cs
+++ Obligatorio-P2-ORT/Dominio/obj/Debug/net8.0/Dominio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dominio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+836227dafa5b3bb17e08967e15774cab766a6570")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3d14dacb78a3ceee9aa3ae6222dd88e5fc3cbbe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dominio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dominio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Obligatorio-P2-ORT/Obligatorio-P2-ORT/obj/Debug/net8.0/Obligatorio-P2-ORT.AssemblyInfo.cs
+++ Obligatorio-P2-ORT/Obligatorio-P2-ORT/obj/Debug/net8.0/Obligatorio-P2-ORT.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Obligatorio-P2-ORT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+836227dafa5b3bb17e08967e15774cab766a6570")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3d14dacb78a3ceee9aa3ae6222dd88e5fc3cbbe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Obligatorio-P2-ORT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Obligatorio-P2-ORT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
